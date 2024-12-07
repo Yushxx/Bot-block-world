3e4c5786c1c97da82f9899c89511f08e4b229046
Yushxx <109989261+Yushxx@users.noreply.github.com>
2024-12-07 18:00:56 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,3 +1,4 @@
+const http = require('http');
 const TelegramBot = require('node-telegram-bot-api');
 
 // Remplacez 'YOUR_API_TOKEN' par le token de votre bot
@@ -74,3 +75,14 @@ bot.on('message', (msg) => {
 
 // Message de démarrage du bot
 console.log("Bot gardien en cours d'exécution...");
+// Créez un serveur HTTP simple qui renvoie "I'm alive" lorsque vous accédez à son URL
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.write("I'm alive");
+    res.end();
+});
+
+// Écoutez le port 8080
+server.listen(8080, () => {
+    console.log("Keep alive server is running on port 8080");
+});
